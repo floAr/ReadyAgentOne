99a7dd2bef51f6aa6ce8ac19b6e5428ea9afc26b
Spencer <spencerobsitnik@gmail.com>
2025-02-06 09:47:48 -0500

    remove more logs

--- agent/superfluid/graphQueries/superfluidGraphQueries.ts
+++ agent/superfluid/graphQueries/superfluidGraphQueries.ts
@@ -8,10 +8,7 @@ const client = new GraphQLClient(BASE_SEPOLIA_GRAPH_ENDPOINT);
 export async function getAccountOutflow(userId: string): Promise<SuperfluidAccountResponse | undefined> {
     try {
         const variables = { id: userId.toLowerCase() };
-        console.log('got id: ' + userId)
         const data = await client.request<SuperfluidAccountResponse>(getAccountOutflowQuery, variables);
-        console.log('got data: ')
-        console.log(data)
         return data;
     } catch (error) {
         console.error('Error fetching account data:', error);
